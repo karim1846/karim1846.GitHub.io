051b4b334388bc584d79a7fa34947dbe2cc89dd0
karim1846 <114772363+karim1846@users.noreply.github.com>
2022-10-01 15:28:09 +0400

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,6 @@
     //update
 var bob_h5 = "/h5.html";//bob-h5
-var bob_pc = "https://www.on4xgo.com/?agent_code=6342892";//bob-pc
+var bob_pc = "/pc.html";//bob-pc
 var boyu_pc = "/boyupc.html";//boyu-pc
 var boyu_h5 = "/boyuh5.html";//boyu-h5
 var app_pc = "https://www.at0z1.com/?i_code=6342892";//app_pc
