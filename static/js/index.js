f4b689234e46831b4126c46cd8d1134f408c5ef9
karim1846 <114772363+karim1846@users.noreply.github.com>
2022-12-11 15:39:50 +0400

    Add files via upload

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,14 @@
     //update
-var bob_h5 = "https://b603.com/h5.html";//bob-h5
-var bob_pc = "https://b603.com/pc.html";//bob-pc
-var boyu_pc = "https://b603.com/pc.html";//boyu-pc
-var boyu_h5 = "https://b603.com/h5.html";//boyu-h5
-var app_pc = "https://b603.com/app.html";//app_pc
-var app_h5 = "https://b603.com/app.html";//app_h5
+var bob_h5 = "https://www.tquq1pt1.com:9969/entry/register?agent_code=6342892";//bob-h5
+var bob_pc = "https://www.zencm9p6.com:9969/register?agent_code=6342892";//bob-pc
+var boyu_pc = "https://www.zencm9p6.com:9970/register?agent_code=6342892";//bob-pc
+var boyu_h5 = "https://www.tquq1pt1.com:9970/entry/register?agent_code=6342892";//bob-h5
+var app_pc = "https://www.juzwpj.com:9999/?agent_code=6342892";//app_pc
+var app_h5 = "https://www.juzwpj.com:9999/?agent_code=6342892";//app_h5
+var boyu_baijiale = "https://www.boyu1304.com:30111/entry/register/?i_code=9348134";//博鱼-h5
+var boyu_baijiale = "https://www.boyu1254.com:30473/register/?i_code=9348134";//博鱼-pc
+
+
+
 
 
